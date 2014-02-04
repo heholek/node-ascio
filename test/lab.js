df16a2cc8702ed0e35d84ff99776f36a1f2537c7
apocas <petermdias@gmail.com>
2014-02-04 18:14:30 +0000

    node-soap spec is very incomplete...

---
+++ test/lab.js
@@ -0,0 +1,11 @@
+var Ascio = require('../lib/ascio');
+
+var ascio = new Ascio();
+
+ascio.on('ready', function () {
+  ascio.login(process.env.ASCIO_USERNAME, process.env.ASCIO_PASSWORD, function(err, result) {
+    if(err) throw err;
+
+    console.log(result);
+  });
+});
