a2f2ad6d02ce21b5df6486deaf53c991448bcce8
Pedro Dias <petermdias@gmail.com>
2014-03-27 19:47:05 +0000

    searchdomain

--- test/lab.js
+++ test/lab.js
@@ -3,16 +3,16 @@ var Ascio = require('../lib/ascio');
 var ascio = new Ascio(process.env.ASCIO_USERNAME, process.env.ASCIO_PASSWORD);
 
 ascio.on('ready', function () {
-  if(err) throw err;
-  console.log(sessionid);
-
+  /*
   ascio.isDomainAvailable(['adsfafavaf'], ['pt', 'com'], function(err, msg) {
     if(err) throw err;
     console.log(msg);
-
-    ascio.logout(sessionid, function(err, msg) {
-      if(err) throw err;
-      console.log(msg);
-    });
   });
+  */
+  
+  ascio.domainInfo('algarve.bike', function(err, msg) {
+    if(err) throw err;
+    console.log(msg);
+  });
+  
 });
